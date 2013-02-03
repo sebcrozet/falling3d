21c765e22927103146b0ac27a82d8770dd17acea
sebcrozet <developer@crozet.re>
2013-02-03 02:25:30 +0100

    Add show instances.

--- Physics/Falling3d/InertiaTensor3d.hs
+++ Physics/Falling3d/InertiaTensor3d.hs
@@ -14,7 +14,9 @@ import Physics.Falling.Math.Transform
 import Physics.Falling3d.Transform3d
 
 newtype InertiaTensor3d        = InertiaTensor3d        Transform3d
+                                 deriving(Show)
 newtype InverseInertiaTensor3d = InverseInertiaTensor3d Transform3d
+                                 deriving(Show)
 
 instance InertiaTensor InertiaTensor3d InverseInertiaTensor3d Vec3 Transform3d where
   inverseInertia (InertiaTensor3d inertia) = InverseInertiaTensor3d $ inverse inertia
